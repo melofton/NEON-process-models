8db754c55ba7a6cf074d8169857b83d8f9e8b790
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-03-29 18:06:46 -0400

    subset to only sites with an ensemble models

--- R/create_mme.R
+++ R/create_mme.R
@@ -7,7 +7,8 @@ create_mme <- function(forecast_models, # vector of list of model names
                        s3 = s3) # the s3 region to look for the forecasts
   {
   
-  message('generating ensemble for ', forecast_models)
+  message('generating ensemble for ', 
+          paste(forecast_models, sep="' '", collapse=", "))
   
   # How many from each forecast should be sampled
   n_models <- length(forecast_models)
@@ -26,7 +27,8 @@ create_mme <- function(forecast_models, # vector of list of model names
       group_by(site_id) |> 
       # remove sites that contain NAs
       filter(!any(is.na(prediction))) |> 
-      ungroup()
+      ungroup() |> 
+      mutate(model_id = forecast_models[i])
     
     message(forecast_models[i], ' read in')
     
@@ -36,15 +38,15 @@ create_mme <- function(forecast_models, # vector of list of model names
         
         pivot_wider(names_from = parameter,
                     values_from = prediction, 
-                    id_cols = c(datetime, site_id)) |> 
+                    id_cols = c(datetime, site_id, model_id)) |> 
         
-        group_by(site_id, datetime) |> 
+        group_by(site_id, datetime, model_id) |> 
         # sample from the distribution based on the mean and sd
         summarise(prediction = rnorm(sample, mean = mu, sd = sigma)) |> 
         group_by(site_id, datetime) |> 
         # parameter value needs to be character
         mutate(parameter = as.character(row_number()),
-               model_id = ensemble_name, 
+               # model_id = ensemble_name, 
                reference_datetime = forecast_date,
                variable = 'temperature',
                family = 'ensemble')
@@ -54,7 +56,7 @@ create_mme <- function(forecast_models, # vector of list of model names
         distinct(parameter) %>%
         slice_sample(n = sample) %>%
         left_join(., forecast, by = c("parameter")) %>%
-        mutate(model_id = ensemble_name, 
+        mutate(#model_id = ensemble_name, 
                reference_datetime = forecast_date,
                parameter = as.character(parameter)) 
       mme_forecast <- bind_rows(mme_forecast, forecast_sample) 
@@ -62,19 +64,35 @@ create_mme <- function(forecast_models, # vector of list of model names
     
   }
   
-  filename <- paste0('aquatics-', forecast_date, '-', ensemble_name, '.csv.gz')
+  # Check for all sites represented in all models
+  site_model_combinations <- nrow(mme_forecast |> distinct(model_id, site_id))
+  all_combinations <- nrow(mme_forecast |> distinct(model_id)) * nrow(mme_forecast |> distinct(site_id))
+  
+  if (site_model_combinations != all_combinations) {
+    
+    
+    mme_forecast <- mme_forecast |> 
+      complete(site_id, model_id) |> 
+      group_by(site_id) |> 
+      filter(!any(is.na(prediction))) 
+    message('not all sites are represented by all models, subsetting sites')
+    message(paste(unique(mme_forecast$site_id), sep="' '", collapse=", "))
+  }
   
   # need to recode the parameter values so each is unqiue
   mme_forecast <- mme_forecast |> 
     group_by(datetime, site_id) |> 
     mutate(parameter = row_number(),
            family = 'ensemble') |> 
-    ungroup()
+    ungroup() |> 
+    mutate(model_id = ensemble_name)
   
-  if (length(unique(mme_forecast$parameter)) != n) {
+  #Check for all models
+  if (length(unique(mme_forecast$parameter)) != (n_models* round(n / n_models, digits = 0))) {
     stop('you are missing some ensemble members, there may be forecasts missing!')
   }
- 
+  
+  filename <- paste0('aquatics-', forecast_date, '-', ensemble_name, '.csv.gz')
   mme_forecast |>
     readr::write_csv(file.path('./Forecasts/ensembles', filename))
   
