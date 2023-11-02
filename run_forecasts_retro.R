1f1bfc8570417d303a095de42cb30250bdc43e0f
Mary Lofton <melofton@vt.edu>
2023-11-02 16:12:20 -0400

    putting checks in for NAs in NOAA data

--- run_forecasts_retro.R
+++ run_forecasts_retro.R
@@ -4,13 +4,22 @@ library(aws.s3)
 
 # check for any missing forecasts
 message("==== Checking for missed climatology forecasts ====")
-challenge_model_name <- 'procCTMIMonod'
+challenge_model_names <- c('procCTMIMonod','procBlanchardMonod','procBlanchardSteele',
+                           'procCTMISteele','procEppleyNorbergMonod','procEppleyNorbergSteele',
+                           'procHinshelwoodMonod','procHinshelwoodSteele')
 
 
 # Dates of forecasts 
 today <- paste(Sys.Date() - days(2), '00:00:00')
 this_year <- data.frame(date = as.character(paste0(seq.Date(as_date('2023-01-01'), to = as_date(today), by = 'day'), ' 00:00:00')),
-                        exists = NA)
+                        exists_procCTMIMonod = NA,
+                        exists_procBlanchardMonod = NA,
+                        exists_procBlanchardSteele = NA,
+                        exists_procCTMISteele = NA,
+                        exists_procEppleyNorbergMonod = NA,
+                        exists_procEppleyNorbergSteele = NA,
+                        exists_procHinshelwoodMonod = NA,
+                        exists_procHinshelwoodSteele = NA) 
 
 # what forecasts have already been submitted?
 challenge_s3_region <- "data"
@@ -18,17 +27,19 @@ challenge_s3_endpoint <- "ecoforecast.org"
 
 # is that file present in the bucket?
 for (i in 1:nrow(this_year)) {
-  forecast_file <- paste0('aquatics-', as_date(this_year$date[i]), '-', challenge_model_name, '.csv.gz')
+  for(j in 1:length(challenge_model_names)){
+  forecast_file <- paste0('aquatics-', as_date(this_year$date[i]), '-', challenge_model_names[j], '.csv.gz')
   
-  this_year$exists[i] <- suppressMessages(aws.s3::object_exists(object = file.path("raw", 'aquatics', forecast_file),
+  this_year[i,j+1] <- suppressMessages(aws.s3::object_exists(object = file.path("raw", 'aquatics', forecast_file),
                                                                 bucket = "neon4cast-forecasts",
                                                                 region = challenge_s3_region,
                                                                 base_url = challenge_s3_endpoint))
-}
+  }
+  }
 
 # which dates do you need to generate forecasts for?
 missed_dates <- this_year |> 
-  filter(exists == F) |> 
+  filter(if_any(starts_with("exists"), ~ . == F)) |> 
   pull(date) |> 
   as_date()
 
@@ -40,6 +51,12 @@ if (length(missed_dates) != 0) {
     # download the noaa once then apply the forecasts
     source('download_noaa.R')
     message('NOAA downloads complete!')
+    
+    message('checking for NAs in NOAA data')
+    if(any(is.na(noaa_past_mean$air_temperature))) next
+    if(any(is.na(noaa_past_mean$surface_downwelling_shortwave_flux_in_air))) next
+    if(any(is.na(noaa_future_mean$air_temperature))) next
+    if(any(is.na(noaa_future_mean$surface_downwelling_shortwave_flux_in_air))) next
     
     # Script to run forecasts
     source("ignore_sigpipes.R")
